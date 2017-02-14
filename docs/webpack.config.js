edea53da6c2cd8223292bcef537096eadaac6d44
quadrant26 <quadrant26@163.com>
2017-02-14 18:05:04 +0800

    webpack.config

---
+++ docs/webpack.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/2/14.
+ */
+
+module.exports = {
+    entry : __dirname + '/app/entry.js',
+    output : {
+        filename : 'bundle.js',
+        path : __dirname + '/public'
+    }
+}
