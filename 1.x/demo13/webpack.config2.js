81f810849ef4b7fd256b34a7033d1a717ae0bcd3
quadrant26 <quadrant26@163.com>
2017-03-14 10:11:24 +0800

    Vendor chunk

---
+++ 1.x/demo13/webpack.config2.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './main.js'
+    },
+    output: {
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+        })
+    ]
+};
