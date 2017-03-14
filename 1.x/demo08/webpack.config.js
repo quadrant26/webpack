824f94188b872886ec8931edc1a02026089b44f1
quadrant26 <quadrant26@163.com>
2017-03-14 12:03:42 +0800

    html-webpack-plugin && open-browser-webpack-plugin

--- 1.x/demo08/webpack.config.js
+++ 1.x/demo08/webpack.config.js
@@ -1,9 +1,20 @@
-var webpack = require("webpack")
+var webpack = require("webpack");
+var HtmlwebpackPlugin = require("html-webpack-plugin");
+var OpenBrowserPlugin = require("open-browser-webpack-plugin");
 
 module.exports = {
     entry : "./main.js",
     output : {
         //path : __dirname + '/dist',
         filename : 'bundle.js'
-    }
+    },
+    plugins : [
+        new HtmlwebpackPlugin({
+            title : "webpack-demos",
+            filename : 'index.html'
+        }),
+        new OpenBrowserPlugin({
+            url : 'http://localhost:8989'
+        })
+    ]
 }
