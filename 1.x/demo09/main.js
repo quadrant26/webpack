a6d97e0d6514c95fb8e1ed50f3f8cdbfb0531e42
quadrant26 <quadrant26@163.com>
2017-03-14 12:28:45 +0800

    new webpack.DefinePlugin({DEV:...})

--- 1.x/demo09/main.js
+++ 1.x/demo09/main.js
@@ -1,1 +1,5 @@
 document.write('<h1>Hello webpack!</h1>');
+
+if(__DEV__){
+    document.write(new Date());
+}
--- 1.x/demo09/webpack.config.js
+++ 1.x/demo09/webpack.config.js
@@ -1,9 +1,13 @@
 var webpack = require("webpack")
+var devFlagPlugin = new webpack.DefinePlugin({
+    __DEV__ : JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+});
 
 module.exports = {
     entry : "./main.js",
     output : {
         //path : __dirname + '/dist',
         filename : 'bundle.js'
-    }
+    },
+    plugins : [devFlagPlugin]
 }
