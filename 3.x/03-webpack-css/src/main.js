d1d2949639ff6eab25a7b7bfa9d942fdfd4a8d50
quadrant26 <quadrant26@163.com>
2020-01-08 17:44:06 +0800

    对css文件的处理

---
+++ 3.x/03-webpack-css/src/main.js
@@ -0,0 +1,12 @@
+// 1. 使用 CommonJs 规范
+const {add, mul} = require('./js/mathUtil.js')
+
+console.log( add(20, 30));
+console.log( mul(20, 30));
+
+// 2. 使用 ES6 的模块化规范
+import { name, age, height} from "./js/info";
+console.log(name, age, height);
+
+// 3. 依赖 css 文件
+require('./css/normal.css');
---
+++ 3.x/03-webpack-css/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/main.js',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.css$/,
+                // css-loader 只负责将 css 文件进行加载
+                // style-loader 负责将样式添加到 DOM 中
+                // 使用多个 loader 时，是从右向左
+                use: ["style-loader", 'css-loader']
+            }
+        ]
+    }
+}
